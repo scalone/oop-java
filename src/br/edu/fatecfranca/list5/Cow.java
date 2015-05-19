e3293a528ef852a747906e6da70dcd444cf0c8d8
Thiago Scalone <thiago@scalone.com.br>
2015-05-19 20:24:18 -0300

    list5:e1 finished.

--- src/br/edu/fatecfranca/list5/Cow.java
+++ src/br/edu/fatecfranca/list5/Cow.java
@@ -1,5 +1,9 @@
 package br.edu.fatecfranca.list5;
 
 public class Cow extends Mammal {
+	@Override
+	public String speak() {
+		return "muuuuu";
+	}
 }
 
--- src/br/edu/fatecfranca/list5/Dog.java
+++ src/br/edu/fatecfranca/list5/Dog.java
@@ -1,5 +1,23 @@
 package br.edu.fatecfranca.list5;
 
 public class Dog extends Mammal {
+	private String vocabulary = "au au";
+
+	public void setVocabulary(String vocabulary) {
+		this.vocabulary = vocabulary;
+	}
+
+	@Override
+	public String speak() {
+		return vocabulary;
+	}
+
+	public void setBarkHigh() {
+		this.setVocabulary("AU AU");
+	}
+
+	public void setBarkLow() {
+		this.setVocabulary("au au");
+	}
 }
 
