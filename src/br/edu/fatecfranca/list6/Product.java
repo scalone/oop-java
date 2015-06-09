992338b5c9a236e8ac8f56385f2d8d9fc11a1b1a
Thiago Scalone <thiago@scalone.com.br>
2015-06-09 20:32:51 -0300

    List 6.

---
+++ src/br/edu/fatecfranca/list6/Product.java
@@ -0,0 +1,68 @@
+
+package br.edu.fatecfranca.list6;
+import java.util.Random;
+
+public class Product {
+	private int serial;
+	private int volume;
+	private int chance=90;
+	private String state="Not Tested";
+
+	public Product(int serial, int volume) {
+		this.serial = serial;
+		this.volume = volume;
+	}
+
+	public Product() {
+		this(1, 5);
+	}
+
+	private boolean doTest() {
+		Random rn = new Random();
+		int number = rn.nextInt(100) + 1;
+		if (number <= chance) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private void setState(String state) {
+		this.state = state;
+	}
+
+	public boolean testUnit() throws Exception {
+		if (this.state.equals("Not Tested")) {
+			if (doTest()) {
+				this.setState("Approved");
+				return true;
+			} else {
+				this.setState("Not Approved");
+				return false;
+			}
+		} else {
+			throw new Exception("Already Tested");
+		}
+	}
+
+	public void setChance(int chance) {
+		this.chance = chance;
+	}
+
+	public int getChance(int chance) {
+		return this.chance;
+	}
+
+	public int getSerial() {
+		return this.serial;
+	}
+
+	public String getState() {
+		return this.state;
+	}
+
+	public int getVolume() {
+		return this.volume;
+	}
+}
+
